97d8c12ce0125c182441bc454822e3b80b687635
jiengss <eljaypatalinghug@gmail.com>
2026-07-08 01:20:41 +0800

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://smztesmqtbxesdzysntt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtenRlc21xdGJ4ZXNkenlzbnR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMjIzMzMsImV4cCI6MjA5NzY5ODMzM30.m0y0IeiLN-WIoKffV4cjfYDzyt_GOExnMh8GNJPGdbQ'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
